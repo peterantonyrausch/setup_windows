d1c25389fcca3741613843ab2e9bd981434aa115
peterantonyrausch <pantonyr@gmail.com>
2022-07-19 19:36:25 -0300

    Update choco_setup.ps1

--- choco_setup.ps1
+++ choco_setup.ps1
@@ -1,6 +1,6 @@
-$Packages = 'nodejs', 'dotnetcore', 'dotnetcore-runtime', 'tortoisegit', 'git', 'spotify', 'dbeaver', 'vscode', 'visualstudio2019professional', 'postman', 'discord', 'lightshot.install'
+$Packages = '7zip', 'adobereader', 'dbeaver', 'drawio', 'git', 'microsoft-windows-terminal', 'nodejs-lts', 'postman', 'rdmfree', 'sharex', 'tortoisegit', 'visualstudio2022enterprise --package-parameters "--locale en-US"', 'visualstudio2022-workload-netweb', 'vscode', 'yarn', 'wsl2', 'wsl-ubuntu-2004', 'docker-desktop'
 
-ForEach ($PackageName in $Packages)
+ForEach ($Package in $Packages)
 {
-    choco install $PackageName -y
+    choco install $Package -y
 }
